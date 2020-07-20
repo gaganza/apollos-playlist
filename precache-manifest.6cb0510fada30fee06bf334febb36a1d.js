9bee7b1e79154fd75e7c41353c4d47968d3ec338
Gaston Aganza <gaston.aganza@gmail.com>
2020-07-19 22:11:05 -0700

    Updates

---
+++ precache-manifest.6cb0510fada30fee06bf334febb36a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b406e306dde55845995e5adadf7216",
+    "url": "/gaganza/apollos-playlist/index.html"
+  },
+  {
+    "revision": "43ad104286fc41ad18f4",
+    "url": "/gaganza/apollos-playlist/static/css/main.8145b925.chunk.css"
+  },
+  {
+    "revision": "ea948b99f001859c0ee8",
+    "url": "/gaganza/apollos-playlist/static/js/2.28c424cd.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/gaganza/apollos-playlist/static/js/2.28c424cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ad104286fc41ad18f4",
+    "url": "/gaganza/apollos-playlist/static/js/main.7fd899b7.chunk.js"
+  },
+  {
+    "revision": "29f64b29435450572138",
+    "url": "/gaganza/apollos-playlist/static/js/runtime-main.f96dc1ff.js"
+  }
+]);
