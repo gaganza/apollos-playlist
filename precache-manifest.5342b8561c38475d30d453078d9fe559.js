2d7c118ef7c020853e32df13759675acbe6ed1a8
Gaston Aganza <gaston.aganza@gmail.com>
2020-07-19 22:24:10 -0700

    Updates

---
+++ precache-manifest.5342b8561c38475d30d453078d9fe559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709d75905597dda3c2e30588d64efe6c",
+    "url": "/apollos-playlist/index.html"
+  },
+  {
+    "revision": "43ad104286fc41ad18f4",
+    "url": "/apollos-playlist/static/css/main.8145b925.chunk.css"
+  },
+  {
+    "revision": "44e7ba9303e5855c1ede",
+    "url": "/apollos-playlist/static/js/2.d09f37a5.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/apollos-playlist/static/js/2.d09f37a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ad104286fc41ad18f4",
+    "url": "/apollos-playlist/static/js/main.7fd899b7.chunk.js"
+  },
+  {
+    "revision": "d739688d3e1893a98d2e",
+    "url": "/apollos-playlist/static/js/runtime-main.6857d4ca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gaganza/apollos-playlist/precache-manifest.6cb0510fada30fee06bf334febb36a1d.js"
+  "/apollos-playlist/precache-manifest.5342b8561c38475d30d453078d9fe559.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gaganza/apollos-playlist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/apollos-playlist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
