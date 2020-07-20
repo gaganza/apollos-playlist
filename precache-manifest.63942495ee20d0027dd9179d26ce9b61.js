07a6e2fce88f79b0e08bd5de13759944d316bb20
Gaston Aganza <gaston.aganza@gmail.com>
2020-07-20 00:41:24 -0700

    Updates

--- precache-manifest.63942495ee20d0027dd9179d26ce9b61.js
+++ precache-manifest.63942495ee20d0027dd9179d26ce9b61.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2290f3b28f29c3116e3ab3b0b74f4141",
+    "revision": "63e63c58c2077e04b733ec1f5850dd33",
     "url": "/apollos-playlist/index.html"
   },
   {
-    "revision": "aff19cdf4743c5984f65",
+    "revision": "4fd2056ff69bca7b1bac",
     "url": "/apollos-playlist/static/css/main.8145b925.chunk.css"
   },
   {
-    "revision": "44e7ba9303e5855c1ede",
-    "url": "/apollos-playlist/static/js/2.d09f37a5.chunk.js"
+    "revision": "8b7eae31e42217493361",
+    "url": "/apollos-playlist/static/js/2.e5ceefa3.chunk.js"
   },
   {
     "revision": "ef90dec32da5a94b550496958cb808df",
-    "url": "/apollos-playlist/static/js/2.d09f37a5.chunk.js.LICENSE.txt"
+    "url": "/apollos-playlist/static/js/2.e5ceefa3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aff19cdf4743c5984f65",
-    "url": "/apollos-playlist/static/js/main.e0593c4f.chunk.js"
+    "revision": "4fd2056ff69bca7b1bac",
+    "url": "/apollos-playlist/static/js/main.040f0e1a.chunk.js"
   },
   {
     "revision": "d739688d3e1893a98d2e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/apollos-playlist/precache-manifest.b6d94ddca309d18121415f739a781068.js"
+  "/apollos-playlist/precache-manifest.63942495ee20d0027dd9179d26ce9b61.js"
 );
 
 self.addEventListener('message', (event) => {
