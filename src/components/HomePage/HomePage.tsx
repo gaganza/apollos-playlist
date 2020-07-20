bce70d51dbde1ce05c2375819e0f432db8e4765d
Gaston Aganza <gaston.aganza@gmail.com>
2020-07-19 18:55:51 -0700

    Add template homepage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,10 +1,85 @@
-import * as React from 'react';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import SignIn from 'components/SignIn';
 
-import { THomePageProps } from './interfaces';
-class HomePage extends React.PureComponent<THomePageProps> {
-  public render(): JSX.Element {
-    return <div>Homepage</div>;
-  }
+import './styles.scss';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center" style={{ color: 'white' }}>
+      {'Copyright © '}
+      <Link color="inherit" href="https:/apollosplaylist.com/">
+        Apollo's Playlist
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default HomePage;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    backgroundColor: '#1D1E28',
+  },
+  image: {
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1507808973436-a4ed7b5e87c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: '#1D1E28',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#1D1E28',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#1D1E28',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square className={'apollos-playlist__home-page'}>
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5" style={{ color: 'white ' }}>
+            Apollo's Playlist
+          </Typography>
+          <SignIn />
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
