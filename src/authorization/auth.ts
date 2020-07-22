a4981a7a57061a01f5f487bcfa5346f0a3f80953
Gaston Aganza <gaston.aganza@gmail.com>
2020-07-21 20:49:22 -0700

    Add deploy action

--- src/authorization/auth.ts
+++ src/authorization/auth.ts
@@ -10,11 +10,7 @@ export const scopes: string[] = [
 
 export const responseType: string = 'token';
 
-export const clientSecret: string =
-  process.env.NODE_ENV === 'development' ? 'fc20b72c74f74a1c8fd2b30269efe82c' : 'c96b4f7d709c49d6aa0f57f49aa4ab5b';
-
-export const clientId: string =
-  process.env.NODE_ENV === 'development' ? 'a91af95c5d8c4e178920646664034328' : 'a5c714fac13f4b5c924497c3b9e533a2';
+export const clientId: string = process.env.REACT_APP_SPOTIFY_CLIENT_ID!;
 
 export const redirectUri: string =
   process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://gaganza.github.io/apollos-playlist/';
