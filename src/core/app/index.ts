efd90da85eb9af4b6a7fd44bdd2522a330c50420
Gaston Aganza <gaston.aganza@gmail.com>
2021-04-25 14:36:51 -0700

    Clean up App component

--- src/core/app/index.ts
+++ src/core/app/index.ts
@@ -1,34 +1,25 @@
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import SpotifyWebApi from 'spotify-web-api-node';
-import { ThunkDispatch } from 'redux-thunk';
-
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { connect, ConnectedProps } from 'react-redux';
 import App from './App';
 import { fetchUser, closeSnackbar, openSnackbar } from 'common/actions';
-import { IAction, IRootState } from 'common/interfaces';
-import { IDispatchProps, IStateProps } from './interfaces';
-import { SnackbarProps } from '@material-ui/core';
+import { IRootState } from 'common/interfaces';
 
-const mapStateToProps = (state: IRootState): IStateProps => {
+const mapStateToProps = (state: IRootState) => {
   return {
     spotifyWebApi: state.spotifyWebApi,
     user: state.user,
     snackbar: state.snackbar,
-  };
-};
+  }
+}
 
-const mapDispatchToProps = (
-  dispatch: ThunkDispatch<
-    IRootState,
-    null,
-    IAction<SpotifyApi.CurrentUsersProfileResponse> | IAction<unknown> | IAction<Partial<SnackbarProps>>
-  >
-): IDispatchProps => {
-  return {
-    fetchUser: (api: SpotifyWebApi) => dispatch(fetchUser(api)),
-    openSnackbar: (data: Partial<SnackbarProps>) => dispatch(openSnackbar(data)),
-    closeSnackbar: () => dispatch(closeSnackbar()),
-  };
-};
+const mapDispatchToProps = {
+  fetchUser,
+  openSnackbar,
+  closeSnackbar
+}
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+export type TAppProps = ConnectedProps<typeof connector> & RouteComponentProps;
+
+export default connector(withRouter(App));
